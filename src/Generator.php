2122762fc02c2ec79c49bbe37eb1201cabebe17f
Scott Dawson <scott@loyaltycorp.com.au>
2018-02-12 10:34:00 +1100

    Add true random string generation

--- src/Generator.php
+++ src/Generator.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace EoneoPay\Utils;
 
 use EoneoPay\Utils\Interfaces\GeneratorInterface;
+use Exception;
 
 class Generator implements GeneratorInterface
 {
@@ -16,8 +17,27 @@ class Generator implements GeneratorInterface
      */
     public function randomString(?int $length = null): string
     {
-        // Character set without ambiguous characters i, l, o, s, 0, 1, 5
-        $characters = \str_split('2346789abcdefghjkmnpqrtuvwxyzABCDEFGHJKMNPQRTUVWXYZ');
+        try {
+            return $this->generateTrueRandomString($length);
+        } /** @noinspection BadExceptionsProcessingInspection */ catch (Exception $exception) {
+            // It's unlikely exception will be thrown as system is running *nix
+            return $this->generatePseudoRandomString($length);
+        }
+    }
+
+    /**
+     * Generate a pseudo random string
+     *
+     * This method is protected only for testing/code coverage purposes
+     *
+     * @param int|null $length The length of the string to return, defaults to 16
+     *
+     * @return string
+     */
+    private function generatePseudoRandomString(?int $length): string
+    {
+        // Mock characters returned by bin2hex
+        $characters = \str_split('0123456789abcdef');
 
         // Ensure a length exists
         $length = $length ?? 16;
@@ -29,5 +49,28 @@ class Generator implements GeneratorInterface
         }
 
         return \implode('', $random);
+    }
+
+    /**
+     * Generate a truly random string
+     *
+     * @param int|null $length The length of the string to return, defaults to 16
+     *
+     * @return string
+     *
+     * @throws \Exception If not enough entropy can be gathered by \random_bytes or \random_int
+     */
+    protected function generateTrueRandomString(?int $length): string
+    {
+        // Generate a string of random bytes
+        $string = \bin2hex(\random_bytes(512));
+
+        // The length of the string to return
+        $length = $length ?? 16;
+
+        // Get a random start position
+        $start = \random_int(0, \strlen($string) - $length - 1);
+
+        return \substr($string, $start, $length);
     }
 }
