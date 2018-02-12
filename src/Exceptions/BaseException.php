726cab8623eb3fc46db5a00d5c8966794a7b646c
NatePage <nathan.page@loyaltycorp.com.au>
2018-02-12 12:24:46 +1100

    Implemented constructor on BaseException to accept nullable parameters.

--- src/Exceptions/BaseException.php
+++ src/Exceptions/BaseException.php
@@ -5,9 +5,22 @@ namespace EoneoPay\Utils\Exceptions;
 
 use EoneoPay\Utils\Interfaces\BaseExceptionInterface;
 use Exception;
+use Throwable;
 
 abstract class BaseException extends Exception implements BaseExceptionInterface
 {
+    /**
+     * BaseException constructor.
+     *
+     * @param null|string $message
+     * @param int|null $code
+     * @param \Throwable|null $previous
+     */
+    public function __construct(?string $message = null, ?int $code = null, \Throwable $previous = null)
+    {
+        parent::__construct($message ?? '', $code ?? 0, $previous);
+    }
+
     /**
      * Get Error code.
      *
